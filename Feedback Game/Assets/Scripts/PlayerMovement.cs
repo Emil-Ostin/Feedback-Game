930a5ef3905604721ab7a03eeb6b7089194054da
Emil-Ostin <emilostin08@gmail.com>
2025-12-18 09:51:45 +0100

    Reapply "Merge branch 'main' of https://github.com/Emil-Ostin/Feedback-Game"
    This reverts commit 7be9a0413b273ffb9d616a7e2a68b75c6b70d266.

--- Feedback Game/Assets/Scripts/PlayerMovement.cs
+++ Feedback Game/Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,55 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Camera cam;
+
+    [SerializeField] float moveSpeed = 10f;
+
+    InputAction moveAction;
+
+    Vector3 moveVector;
     void Start()
     {
-        
+        moveAction = InputSystem.actions.FindAction ("Move");
+
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+
+        // Vector3 mousePos = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
+        // float angleRad = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x);
+        //float angleDeg = (180 / Mathf.PI) * angleRad - 90; // Offset this by 90 Degrees
+
+        //transform.rotation = Quaternion.Euler(0f, 0f, angleDeg);
+        //Debug.DrawLine(transform.position, mousePos, Color.white, Time.deltaTime);
+
+        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if (mousePos.x > transform.position.x)
+        {
+            // Face right
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            // Face left
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+
+    void MovePlayer()
+    {
+        moveVector = moveAction.ReadValue<Vector2>();
+        transform.position += moveVector * moveSpeed * Time.deltaTime;
+
+      
+     
     }
 }
