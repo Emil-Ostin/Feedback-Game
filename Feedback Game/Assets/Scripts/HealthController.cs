62276e81500117adeeef2b7e540e37b9241d441d
Emil-Ostin <emilostin08@gmail.com>
2026-02-19 10:23:52 +0100

    Parallax complete

--- Feedback Game/Assets/Scripts/HealthController.cs
+++ Feedback Game/Assets/Scripts/HealthController.cs
@@ -2,19 +2,40 @@ using UnityEngine;
 
 public class HealthController : MonoBehaviour
 {
+    [Header("Parameters")]
     [SerializeField] int startHealth;
+
+    [Header("Live Stats")]
     public int currentHealth;
     public bool isDead = false;
+    public bool takenDamage;
+
+    int takenDamageAmount;
 
     private void Awake()
     {
         currentHealth = startHealth;
     }
 
+    private void FixedUpdate()
+    {
+        TakenDamage();
+
+        takenDamageAmount = 0;
+    }
+
     public void Health(int damageAmount)
     {
         currentHealth -= damageAmount;
 
+        takenDamageAmount = damageAmount;
+
         if (currentHealth <= 0) { isDead = true; }
+    }
+
+    public void TakenDamage()
+    {
+        if (takenDamageAmount > 0) { takenDamage = true; }
+        else { takenDamage = false; }
     }
 }
